255f845ca35fc1eb37e0c8f3aff2ef3e51750757
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-26 13:50:31 +0530

    Create colors.ts

---
+++ pkg/webui/src/components/colors.ts
@@ -0,0 +1,5 @@
+export const ColorSuccess = '#2EC990';
+export const ColorFailure = '#F75E60';
+export const ColorWarning = '#FCB008';
+export const ColorUnknown = '#CCCCCC';
+export const ColorRunning = '#A3B2BD';
