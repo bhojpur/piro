c1859c9c37b821002512b4127b48b90ad96e8d27
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-26 13:54:39 +0530

    Create util.ts

---
+++ pkg/webui/src/components/util.ts
@@ -0,0 +1,17 @@
+import { JobPhase, JobPhaseMap } from "../api/piro_pb";
+
+export function debounce<T>(f: (a: T) => void, interval: number): (a: T) => void {
+    let tc: any | undefined;
+
+    return (a: T) => {
+        if (tc !== undefined) {
+            clearTimeout(tc);
+        }
+        tc = setTimeout(() => f(a), 200);
+    }
+}
+
+export function phaseToString(p: JobPhaseMap[keyof JobPhaseMap]): string {
+    const kvs = Object.getOwnPropertyNames(JobPhase).map(k => [k, (JobPhase as any)[k]]).find(kv => kv[1] === p);
+    return kvs![0].split("_")[1].toLowerCase();
+}
